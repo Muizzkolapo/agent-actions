a6539c7126c1518d0686c5ff7e4371f24fd6cda3
Muizz Lateef <muizz@192.168.1.245>
2024-10-12 16:13:54 +0100

    added-all

--- agent_actions/vendors/openai_vendor.py
+++ agent_actions/vendors/openai_vendor.py
@@ -3,6 +3,8 @@ from textwrap import dedent
 from openai import OpenAI
 import logging
 import os 
+from agent_actions.core.utils import process_as_string,ensure_list
+
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -15,23 +17,22 @@ class OpenAIHandler:
 
         model_name = agent_config['model_name']
 
+
+        input_documentation_str = process_as_string(input_documentation)
+
         prompt = f"""
             <|begin_of_user_instruction|>: {prompt_config} :<|end_of_user_instruction|>
+            <|begin_of_text|>: {str(input_documentation_str)} :<|end_of_text|>
             <|begin_of_output_schema|> : list of this [{schema}] : <|end_of_output_schema|>
 
             RULES: YOU CANNOT RETURN THE CONTENT OF OUTPUT SCHEMA IN YOUR OUTPUT
             RULES: ALWAYS READ INPUT AS STRING
-            RULES: return list of schema object
         """
 
         messages = [
             {
                 "role": "system",
                 "content": dedent(prompt)
-            },
-            {
-                "role": "user",
-                "content": input_documentation
             }
         ]
 
