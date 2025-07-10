c72482e8978981c2a54793e3ea77fa55492574f3
Muizz Lateef <muizz@Muizzs-MacBook-Pro.local>
2025-07-10 22:32:09 +0100

    done

--- agent_actions/processors/target_processor/target_content_processor.py
+++ agent_actions/processors/target_processor/target_content_processor.py
@@ -1,5 +1,6 @@
 """Module for processing target content with specialized components."""
 from typing import Dict, List, Tuple
+import json
 from agent_actions.services.batch_service import BatchService
 from agent_actions.transformers.data_transformer import DataTransformer
 
@@ -153,7 +154,13 @@ class TargetContentProcessor(IContentProcessor):
             
             # Get corresponding source content
             source_content = DataTransformer.get_content_by_guid(source_data, guid)
-            
+
+            #==================temp fix=========
+            source_content = [
+                s for s in json.dumps(source_content, ensure_ascii=False).split('"')
+                if r'\x' in s              # r'\x' prevents the interpreter from parsing it
+            ]  
+            #==================temp fix=========         
             # Generate data through the shared utility
             generated_data, executed = self.data_generator.create_agent_with_data(
                 contents, source_content
